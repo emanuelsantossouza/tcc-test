5aeff1a4268a73e69f734b836d40993db01ccfc8
emanuel santoss <emanuelsantoss42@gmail.com>
2023-03-03 17:03:52 -0300

    atualizações

---
+++ teste api/testchamaai/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'menus',
+    loadChildren: () => import('./components/menus/menus.module').then( m => m.MenusPageModule)
+  },
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
