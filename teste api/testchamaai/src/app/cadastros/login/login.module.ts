65c0844cf57e4decc7d19f23a7f5994087b5f56f
emanuel santoss <emanuelsantoss42@gmail.com>
2023-03-10 17:57:36 -0300

    Tela do login 90% pronta

--- teste api/testchamaai/src/app/cadastros/login/login.module.ts
+++ teste api/testchamaai/src/app/cadastros/login/login.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { ExploreContainerComponentModule } from 'src/app/explore-container/explore-container.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    ExploreContainerComponentModule
   ],
   declarations: [LoginPage]
 })
